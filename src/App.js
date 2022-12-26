61591a4b11bdd33bc506c8abcbbbaa79ca11f0af
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2022-12-26 17:18:55 +0100

    add Ball and improve its look with css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Ball from './Ball';
 
 function App() {
   return (
     <div className="App">
+      <Ball num={12} />
+      <Ball num={7} />
+      <Ball num={19} />
+      <Ball num={25} />
     </div>
   );
 }
